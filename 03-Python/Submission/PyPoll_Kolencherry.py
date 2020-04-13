f614a4f708810edd75c68ad26020069443bb277c
Terisha Kolencherry <tkolencherry@Terishas-Air.attlocal.net>
2020-04-12 22:13:16 -0500

    PyPoll clean code + text file

--- 03-Python/Submission/PyPoll_Kolencherry.py
+++ 03-Python/Submission/PyPoll_Kolencherry.py
@@ -7,7 +7,11 @@ csvpath = r"Instructions/PyPoll/Resources/election_data.csv"
  
 #Set count of months equal to zero to start
 vote_count = 0 
+
+#define array for candidates to store candidate names
 candid= []
+
+#candidate vote totals, set to zero 
 Khan_t = 0 
 Li_t = 0 
 Correy_t = 0 
@@ -16,18 +20,20 @@ Tooley_t = 0
 #Read in CSV file 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    #Changed to import as dictionary so no need to skip the header - if reading in regularly then
-        #include code below
 
+    #skip header
     if csv.Sniffer().has_header:
         next(csvreader)
 
     #for loop to go through each row 
     for row in csvreader:
+
         #add the row to the count to get the number of votes 
         vote_count += 1
+
         #add the value into a distinct list of candidate names
         candid.append(row[2])
+
         #tally votes for each candidate
         if row[2] == "Khan": 
             Khan_t += 1 
@@ -38,34 +44,37 @@ with open(csvpath) as csvfile:
         elif row[2] == "O'Tooley":
             Tooley_t += 1
 
-    #get the set of the index in the csv to get unique values of candidate names 
+    #put the candidate names into an array 
     array = np.array([candid])
+
+    #return the unique candidate names and the frequency in which they occur in the data set
     candidates,counts = np.unique(array, return_counts=True)
-    zippy = zip(candidates, counts)
-    dic_zip = dict(zippy)
 
-    #percentages and winner
+    #put the unique values into a dictionary 
+    vote_summary = dict(zip(candidates, counts))
+
+    #find the percentages of each vote using lambda function for x s.t. x/total and x = vote count
     percents = list(map(lambda x:round(float(x)/vote_count*100,2),counts))
+
+    #zip to dictionary 
     percents_dict = dict(zip(candidates, percents))
-    winner = max(dic_zip,key = dic_zip.get)
+
+    #find winner through max, only need to print the candidate
+    winner = max(vote_summary,key = vote_summary.get)
     
+    #create a text file
+    results = open("PyPoll Results_Kolencherry.txt","w")
+
     #print print print 
-    print(f'The total number of votes was {vote_count} .') 
-    print(f'Candidate vote totals were as follows: {dic_zip} .')
-    print(f'The candidate percents were as follows : {percents_dict} .')
-    print(f'The winner of the popular vote is  {winner} .')
+    print(f'The total number of votes was {vote_count}.', file = results) 
+    print(f'Candidate vote totals were as follows: {vote_summary}.', file = results)
+    print(f'The candidate percents were as follows : {percents_dict}.', file = results)
+    print(f'The winner of the popular vote is  {winner}.', file = results)
+
+    results.close()
 
     
     
 
 
 
-    #* The total number of votes cast
-
-    #* A complete list of candidates who received votes
-
-    #* The percentage of votes each candidate won
-
-    #* The total number of votes each candidate won
-
-    #* The winner of the election based on popular vote.
