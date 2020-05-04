a0308108c2c1e44d5d256e800beb82c5f8e43d05
Terisha Kolencherry <tkolencherry@Terishas-Air.attlocal.net>
2020-05-03 21:41:56 -0500

    fixed the api key issue

--- 06-Python-APIs/Instructions/Submission/api_keys.py
+++ 06-Python-APIs/Instructions/Submission/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "f9f6e9be6eee8027393a7ee6088f3e9d"
+weather_api_key = ''
 
 # Google API Key
-g_key = "AIzaSyCR9sNOdX6-pJKHrFPZcLckt4NxySn1n0s"
+g_key = ''
